6cb3b95237049916e3afdcc9e2accebace58abd6
Prajwal Kusha <prajwalkusha.uni@gmail.com>
2025-05-29 17:27:53 -0400

    Update indeed_scraper.py

--- backend/indeed_scraper.py
+++ backend/indeed_scraper.py
@@ -1,6 +1,6 @@
 import undetected_chromedriver as uc
-# import chromedriver_autoinstaller 
-# chromedriver_autoinstaller.install() 
+import chromedriver_autoinstaller 
+chromedriver_autoinstaller.install() 
 from bs4 import BeautifulSoup
 import json
 import time
@@ -42,12 +42,16 @@ class IndeedScraper:
             options.add_argument("--headless=new")  # ADD THIS LINE (ensure headless on cloud)
 
             # Create the driver with minimal options
-            options.binary_location = os.getenv("GOOGLE_CHROME_BIN", "/usr/bin/chromium-browser")
+            # options.binary_location = os.getenv("GOOGLE_CHROME_BIN", "/usr/bin/chromium-browser")
             self.driver = uc.Chrome(
                 options=options,
-                driver_executable_path=os.getenv("CHROMEDRIVER_PATH", "/usr/bin/chromedriver"),
                 use_subprocess=True,
                 version_main=None,
+            # self.driver = uc.Chrome(
+            #     options=options,
+            #     driver_executable_path=os.getenv("CHROMEDRIVER_PATH", "/usr/bin/chromedriver"),
+            #     use_subprocess=True,
+            #     version_main=None,
             )
 
             # Set up wait after driver is created
