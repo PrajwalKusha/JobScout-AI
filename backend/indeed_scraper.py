6e7ef9c9c551a0eb82c58aeec2a5ebd6d97bb260
Prajwal Kusha <prajwalkusha.uni@gmail.com>
2025-05-29 11:20:17 -0400

    Update indeed_scraper.py

--- backend/indeed_scraper.py
+++ backend/indeed_scraper.py
@@ -13,7 +13,7 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import List, Dict, Optional
 from utils.logger import job_scraper_logger
 from utils.exceptions import ScrapingError, BrowserError, RateLimitError, JobScraperError
-import chromedriver_autoinstaller
+# import chromedriver_autoinstaller
 import os 
 
 class IndeedScraper:
